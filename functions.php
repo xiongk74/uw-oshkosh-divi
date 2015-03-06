3d8fab3af5bf157838c38bc3405c9c34986180c5
Shawn Hansen <shawn.hansen@gmail.com>
2015-03-06 08:14:59 -0600

    Prefix function names.

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 
 // Stylesheets
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
+function uwo_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array('parent-style')
     );
 }
+add_action( 'wp_enqueue_scripts', 'uwo_theme_enqueue_styles' );
 
 ?>
