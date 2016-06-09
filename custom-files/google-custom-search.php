ec6b54043263c26d5900c6b01d1af7d42ba5e05c
Joseph Kerkhof <joseph.kerkhof@gmail.com>
2016-06-09 15:51:24 -0500

    Cleaned the biogas search results out

--- custom-files/google-custom-search.php
+++ custom-files/google-custom-search.php
@@ -1,14 +1,3 @@
 <?php
-  $gcs = "<script>
-    (function() {
-      var cx = '007334596040304417000:lolcarafb1s';
-      var gcse = document.createElement('script');
-      gcse.type = 'text/javascript';
-      gcse.async = true;
-      gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(gcse, s);
-    })();
-  </script>
-  <gcse:searchbox-only resultsUrl='http://biogas.uwosh.edu/search-results/'></gcse:searchbox-only>";
+  $gcs = "";
 ?>
