050c636a8f9332599d38fce10e774f15797fd957
Joseph Kerkhof <joseph.kerkhof@gmail.com>
2016-07-08 14:58:29 -0500

    Updates to favicon and optimized screenshot.png

--- theme/uw-oshkosh-divi/functions.php
+++ theme/uw-oshkosh-divi/functions.php
@@ -42,23 +42,23 @@ add_action( 'login_enqueue_scripts', 'my_login_logo' );
 // Favicons
 function uwo_favicon_link() {
     echo '<!-- Favicons -->
-    <link rel="shortcut icon" type="image/x-icon" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon.ico">
-    <link rel="apple-touch-icon" sizes="57x57" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-152x152.png">
+    <link rel="shortcut icon" type="image/x-icon" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon.ico?v=2">
+    <link rel="apple-touch-icon" sizes="57x57" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-57x57.png?v=2">
+    <link rel="apple-touch-icon" sizes="114x114" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-114x114.png?v=2">
+    <link rel="apple-touch-icon" sizes="72x72" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-72x72.png?v=2">
+    <link rel="apple-touch-icon" sizes="144x144" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-144x144.png?v=2">
+    <link rel="apple-touch-icon" sizes="60x60" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-60x60.png?v=2">
+    <link rel="apple-touch-icon" sizes="120x120" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-120x120.png?v=2">
+    <link rel="apple-touch-icon" sizes="76x76" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-76x76.png?v=2">
+    <link rel="apple-touch-icon" sizes="152x152" href="' . get_stylesheet_directory_uri() . '/img/favicons/apple-touch-icon-152x152.png?v=2">
     <meta name="apple-mobile-web-app-title" content="UW Oshkosh">
-    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon-196x196.png" sizes="196x196">
-    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon-160x160.png" sizes="160x160">
-    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon-16x16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon-196x196.png?v=2" sizes="196x196">
+    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon-160x160.png?v=2" sizes="160x160">
+    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon-96x96.png?v=2" sizes="96x96">
+    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon-16x16.png?v=2" sizes="16x16">
+    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/img/favicons/favicon-32x32.png?v=2" sizes="32x32">
     <meta name="msapplication-TileColor" content="#ffc40d">
-    <meta name="msapplication-TileImage" content="' . get_stylesheet_directory_uri() . '/img/favicons/mstile-144x144.png">
+    <meta name="msapplication-TileImage" content="' . get_stylesheet_directory_uri() . '/img/favicons/mstile-144x144.png?v=2">
     <meta name="application-name" content="UW Oshkosh">';
 }
 add_action( 'wp_head', 'uwo_favicon_link' );
