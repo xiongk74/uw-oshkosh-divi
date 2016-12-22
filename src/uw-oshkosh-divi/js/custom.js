c193d5797cff342f864d714d7e880627fc12682d
Taylor1337 <taylor.korthals@gmail.com>
2016-12-22 11:32:35 -0600

    Fixes to menu backgrounds and mobile menu
    Fixes problem with menu background being black, and overlapping text in
    the mobile menu

--- src/uw-oshkosh-divi/js/custom.js
+++ src/uw-oshkosh-divi/js/custom.js
@@ -1,6 +1,6 @@
-// Wordpress uses the 'jQuery' object by default to call jQuery.
+// Wordpress uses the '$' object by default to call $.
 //  This wrapping, self-executing function allows us to use the
-//  default $ for all of our jQuery calls.
+//  default $ for all of our $ calls.
 (function ($) {
   $(document).ready(function(){
     // Custom search box
@@ -14,5 +14,28 @@
     // (prevents the main menu from dropping down when a sub-menu
     // item is clicked)
     $(".et_pb_fullwidth_section").find("li.menu-item > a").off("click");
-  });
+    //fixes dropdown menu from having white background-color
+    function menu_background()
+    {
+      var nameOfMenu = $(".et_pb_fullwidth_menu");
+      for ( let name of nameOfMenu){
+        var classname= $(name).attr("class");
+        var splitClassName= classname.split(" ");
+        var lastClass = splitClassName.pop();
+        $("." + lastClass ).children().children().find("*").css('background-color', $("."+lastClass).css('background-color'));
+      }
+      }
+
+      menu_background();
+      //stops the mobile menu items from overlapping
+      function menu_Overlap(){
+        var menus = $(".et_mobile_menu").find('*');
+        for (let item of menus){
+          if(item.firstElementChild==null){
+            $(item).addClass("mobile-menu-text");
+          }
+        }
+      }
+      menu_Overlap();
+    });
 }(jQuery));
