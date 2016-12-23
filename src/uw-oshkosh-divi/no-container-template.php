2248ae79a4c9b5fb9920dede5dfba70cdcb0442c
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-12-23 08:38:17 -0600

    Adding a new page template

---
+++ src/uw-oshkosh-divi/no-container-template.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: No Container (Header, Footer, No Sidebar)
+ */
+get_header(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post();?>
+  <?php the_content(); ?>
+<?php endwhile; endif; ?>
+<?php
+get_footer();
