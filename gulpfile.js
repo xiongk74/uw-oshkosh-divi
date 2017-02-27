88f8b36f92d080d108fa1f65dbba6056f0186115
Joseph Kerkhof <musicaljoeker@gmail.com>
2017-02-27 17:20:17 -0600

    Adding the UWO WordPress Bundle to the project.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const gulp = require('gulp');
 // Include Our Plugins
 const jshint = require('gulp-jshint');
 const sass = require('gulp-sass');
-const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const copy = require('gulp-copy');
@@ -16,19 +15,26 @@ const mkdirp = require('gulp-mkdirp');
 const clean = require('gulp-clean');
 const runSequence = require('run-sequence');
 const autoprefixer = require('gulp-autoprefixer');
-const cssnano = require('gulp-cssnano');
+const cleanCSS = require('gulp-clean-css');
 const gulpif = require('gulp-if');
 const scsslint = require('gulp-scss-lint');
 const browserSync = require('browser-sync').create();
 const watch = require('gulp-watch');
 
-// Including the path to local WP development
-var localWPConfig = require('./config/local-wp-config.js');
+gulp.task('serve', function(){
+  // Building and piping the files before browserSync init
+  runSequence('build-theme', 'bs-init');
+
+  // Watching all files in the src
+  return watch('src/**/*.*', function(){
+    runSequence('build-theme', 'bs-reload');
+  });
+});
 
 // Tasks for local WordPress development
 gulp.task('bs-init', function(){
   browserSync.init({
-      proxy: localWPConfig.proxy
+      proxy: 'localhost:8080'
   });
 });
 
@@ -36,82 +42,123 @@ gulp.task('bs-reload', function (){
     browserSync.reload();
 });
 
-gulp.task('pipe-files', function(){
-  return gulp.src('builds/**/*.*')
-          .pipe(gulp.dest(localWPConfig.filePath));
+gulp.task('default', function(cb){
+  runSequence('build-theme');
 });
 
-gulp.task('serve', function(){
-  // Building and piping the files before browserSync init
-  runSequence('build', 'pipe-files', 'bs-init');
-
-  // Watching all files in the src
-  return watch('src/uw-oshkosh-divi/**/*.*', function(){
-    runSequence('build', 'pipe-files', 'bs-reload');
-  });
+gulp.task('build-theme', function(cb) {
+  runSequence('clean-theme',
+    ['style-theme', 'js-theme', 'images-theme', 'php-theme'],
+    ['js-theme-lint', 'theme-scss-lint'],
+    cb);
 });
 
 // Tasks for WordPress theme build
-gulp.task('clean', function() {
-    return gulp.src('builds')
+gulp.task('clean-theme', function() {
+    return gulp.src(['dist/wp-content/themes/*', '!dist/wp-content/themes/index.php', '!dist/wp-content/themes/Divi'])
         .pipe(clean());
 });
 
 // Building the CSS
-gulp.task('style', function () {
-  // Function to check if file contains WordPress theme metadata
-  var notThemeMetadata = function(file){
-    var filenameSplit = String(file["history"]).split('/');
-    var filename = filenameSplit[filenameSplit.length-1];
-    if(filename != 'theme-metadata.css'){
-      return true;
-    }
-    return false;
-  }
-
-  return gulp.src(['src/uw-oshkosh-divi/theme-metadata.css', 'src/uw-oshkosh-divi/style/*.scss'])
+gulp.task('style-theme', function () {
+  return gulp.src('src/themes/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
-    .pipe(gulpif(notThemeMetadata, cssnano()))
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('builds/uw-oshkosh-divi'));
+    .pipe(cleanCSS({
+          keepSpecialComments: 1
+      }))
+    .pipe(gulp.dest('dist/wp-content/themes/'));
 });
 
-gulp.task('js', function(){
-  return gulp.src('src/uw-oshkosh-divi/js/**/*.js')
+gulp.task('js-theme', function(){
+  return gulp.src('src/themes/**/js/**/*.js')
       .pipe(babel({
         presets: ['es2015']
       }))
       .pipe(uglify())
-      .pipe(gulp.dest('builds/uw-oshkosh-divi/js'));
+      .pipe(gulp.dest('dist/wp-content/themes/'));
 });
 
-gulp.task('images', function(){
-  return gulp.src('src/uw-oshkosh-divi/img/**/*')
+gulp.task('images-theme', function(){
+  return gulp.src('src/themes/**/images/**/*')
       .pipe(imagemin())
-      .pipe(gulp.dest('builds/uw-oshkosh-divi/img'));
+      .pipe(gulp.dest('dist/wp-content/themes/'));
 });
 
-gulp.task('php', function(){
-  return gulp.src('src/uw-oshkosh-divi/**/*.php')
-      .pipe(gulp.dest('builds/uw-oshkosh-divi/'));
+gulp.task('php-theme', function(){
+  return gulp.src('src/themes/**/**/*.php')
+      .pipe(gulp.dest('dist/wp-content/themes/'));
 });
 
-gulp.task('js-lint', function(){
-  return gulp.src('src/uw-oshkosh-divi/js/*.js')
+gulp.task('js-theme-lint', function(){
+  return gulp.src('src/themes/**/js/*.js')
       .pipe(jshint({
         'esversion': 6
       }))
       .pipe(jshint.reporter('default'));
 });
 
-gulp.task('scss-lint', function() {
-  return gulp.src('src/uw-oshkosh-divi/style/*.scss')
+gulp.task('theme-scss-lint', function() {
+  return gulp.src('src/themes/**/style/*.scss')
     .pipe(scsslint({'config': 'config/scss-lint-config.yml'}));
 });
 
-gulp.task('build', function(cb) {
-  runSequence('clean', ['style', 'js', 'images', 'php'], ['js-lint', 'scss-lint'], cb);
-});
+
+// // Tasks for the WordPressplugin plugin build
+// gulp.task('build-plugin', function(cb) {
+//   runSequence('plugin-clean', ['plugin-style', 'plugin-js', 'plugin-images', 'plugin-php'], ['plugin-js-lint', 'plugin-scss-lint'], cb);
+// });
+//
+// // Tasks for WordPress theme build
+// gulp.task('plugin-clean', function() {
+//     return gulp.src('dist/wp-content/plugins/')
+//         .pipe(clean());
+// });
+//
+// // Building the CSS
+// gulp.task('plugin-style', function () {
+//   return gulp.src('src/plugins/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(autoprefixer({
+//       browsers: ['last 2 versions']
+//     }))
+//     .pipe(cleanCSS({
+//           keepSpecialComments: 1
+//       }))
+//     .pipe(gulp.dest('dist/wp-content/plugins/'));
+// });
+//
+// gulp.task('plugin-js', function(){
+//   return gulp.src('src/plugins/**/js/**/*.js')
+//       .pipe(babel({
+//         presets: ['es2015']
+//       }))
+//       .pipe(uglify())
+//       .pipe(gulp.dest('dist/wp-content/plugins/'));
+// });
+//
+// gulp.task('plugin-images', function(){
+//   return gulp.src('src/plugins/**/img/**/*')
+//       .pipe(imagemin())
+//       .pipe(gulp.dest('dist/wp-content/plugins/'));
+// });
+//
+// gulp.task('plugin-php', function(){
+//   return gulp.src('src/plugins/**/**/*.php')
+//       .pipe(gulp.dest('dist/wp-content/plugins/'));
+// });
+//
+// gulp.task('plugin-js-lint', function(){
+//   return gulp.src('src/plugins/**/js/*.js')
+//       .pipe(jshint({
+//         'esversion': 6
+//       }))
+//       .pipe(jshint.reporter('default'));
+// });
+//
+// gulp.task('plugin-scss-lint', function() {
+//   return gulp.src('src/plugins/**/style/*.scss')
+//     .pipe(scsslint({'config': 'config/scss-lint-config.yml'}));
+// });
